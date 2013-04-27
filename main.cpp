cd03fa7aaaa53bc39b2728839d616fecfb21c40b
Amandeep Singh <aman.dedman@gmail.com>
2013-04-27 22:08:59 +0530

    Ussd and SMS working
      Checks for balance by sending Ussd message. Reads first sms
      from device. Sends Hello world text now.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <QApplication>
 #include <QDebug>
-#include <QtDBus/QDBusConnection>
-#include <QtDBus/QDBusMessage>
-#include <QtDBus/QDBusReply>
-#include <QtDBus/QDBusInterface>
+#include <QMetaType>
+#include <QtDBus>
+#include <QVariant>
 
 int main(int argc, char **argv)
 {
@@ -58,6 +57,42 @@ int main(int argc, char **argv)
           QDBusConnection::systemBus().call(enableModem);
           enabled = modemInterface.property("Enabled").toBool();
           qDebug() << "Modem Enabled: " << enabled;
+        }
+
+        QDBusInterface modemUssdInterface(
+            "org.freedesktop.ModemManager",
+            modemPath.path(),
+            "org.freedesktop.ModemManager.Modem.Gsm.Ussd",
+            QDBusConnection::systemBus());
+
+        QString ussdState = modemUssdInterface.property("State").toString();
+        if(ussdState != "idle")
+          modemUssdInterface.call("Cancel");
+
+        qDebug() << "Dialing *444# to check balance";
+        QDBusReply<QString> balanceMessage = modemUssdInterface.call("Initiate","*444#");
+        qDebug() << balanceMessage;
+
+        QDBusInterface modemSMSInterface(
+            "org.freedesktop.ModemManager",
+            modemPath.path(),
+            "org.freedesktop.ModemManager.Modem.Gsm.SMS",
+            QDBusConnection::systemBus());
+
+        qDebug() << "Displaying first SMS on the SIM card";
+        QDBusReply < QVariantMap >  firstSMS = modemSMSInterface.call("Get",uint(0));
+        qDebug() << firstSMS.value()["text"].toString();
+
+        qDebug() << "Sending my first SMS Hello World to +919013380180";
+        QVariantMap helloWorld;
+        helloWorld["number"]="+919013380180";
+        helloWorld["text"]="Hello World!";
+        QDBusReply< QList<QVariant> > helloWorldSent = modemSMSInterface.call("Send",helloWorld);
+
+        if(helloWorldSent.isValid()) {
+          qDebug() << helloWorldSent.value();
+        } else {
+          qWarning() << "Unable to send message |= " << helloWorldSent.error().message();
         }
       }
     }
