fcf091829842191638fd24929331cc7cc5d9ad81
Amandeep Singh <aman.dedman@gmail.com>
2013-04-27 17:48:48 +0530

    Enumerating all modems, displaying path of first

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <QApplication>
+#include <QDebug>
 #include <QtDBus/QDBusConnection>
+#include <QtDBus/QDBusMessage>
+#include <QtDBus/QDBusReply>
 
 int main(int argc, char **argv)
 {
@@ -13,6 +16,27 @@ int main(int argc, char **argv)
 
     qDebug() << "Connected to System Bus";
   
+    QDBusMessage enumerateDevices = QDBusMessage::createMethodCall(
+        "org.freedesktop.ModemManager",
+        "/org/freedesktop/ModemManager",
+        "org.freedesktop.ModemManager",
+        "EnumerateDevices");
+    QDBusReply< QList<QDBusObjectPath> > reply = QDBusConnection::systemBus().call(enumerateDevices);
+
+    if(!reply.isValid()) {
+      qWarning() << "Could not enumerate devices";
+      qWarning() << reply.error().message();
+
+    } else {
+
+      if(reply.value().size()==0) {
+        qWarning() << "No Modem Found";
+
+      } else {
+        QDBusObjectPath modemPath = reply.value()[0];
+        qDebug() << "Device Found at : " << modemPath.path();
+      }
+    }
   }
 
   return app.exec();
